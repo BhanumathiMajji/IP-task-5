c662c70b43633c7f9186f8968be5f402d803d100
BhanumathiMajji <118416242+BhanumathiMajji@users.noreply.github.com>
2023-07-23 21:51:03 +0530

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Main{  
+public static void main(String[] args) {  
+        int [] arr = new int [] {1, 2, 3, 2,7, 2, 1, 9, 8, 3};  
+        System.out.println("Duplicate elements are- ");   
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
+    }  
+}
